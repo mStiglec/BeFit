df3486a7ec458c5abb9032fa912efe0224f6d40e
mStiglec <sticgoc@gmail.com>
2022-08-01 22:45:34 +0200

    add jetpac compose

--- BeFit/app/src/main/java/com/example/befit/MainActivity.kt
+++ BeFit/app/src/main/java/com/example/befit/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.befit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
+    @Composable
+    fun ShowName(name: String){
+        Text(text = "Hello $name")
+    }
+
+    @Composable
+    @Preview
+    fun PreviewMyName(){
+        ShowName("Miky")
+    }
+
+
 }
